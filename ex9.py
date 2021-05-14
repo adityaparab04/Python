480916efe9e2b98666ae9f2efba26b974fe818bf
adityaparab04 <adityaparab04@gmail.com>
2021-05-14 13:49:26 +0530

    ex9 commit

---
+++ ex9.py
@@ -0,0 +1,11 @@
+days = "Mon Tue Wed Thu Fri Sat" 
+months = "\nJan\nFeb\nMa\nApr\nMay\nJune\nJul\nAug"
+
+print("Here are days", days)
+print("Here are months", months)
+print("""
+There is something going on here.
+with the three double quotes.
+We'll be able to type as much as we like.
+Even 4 lines if we want to, or more.
+""")
