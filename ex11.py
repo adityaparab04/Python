6ddcfb75b399d0897f8ec251c08d1d06c3864f40
adityaparab04 <adityaparab04@gmail.com>
2021-05-15 11:49:44 +0530

    new ex commit

---
+++ ex11.py
@@ -0,0 +1,20 @@
+print ("How old are you?", end = ' ')
+age = input()
+print ("How tall are you?", end = ' ')
+height = input()
+print ("How much do you Weigh?", end = ' ')
+weight = input()
+
+print(f"So you're {age} old, {height} tall and {weight} heavy.")
+#another example
+
+print ("Where do you study")
+edu = input()
+print ("what is your gpa ")
+gpa = input()
+print (f"You study in {edu} and your gpa is {gpa}")
+
+x = float(input())
+y = int(input())
+z = x+y
+print ("x + y = ", z ) 
