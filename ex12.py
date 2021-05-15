3cbc2b089cccdc8403b95abd3e417fb767961801
adityaparab04 <adityaparab04@gmail.com>
2021-05-15 12:15:44 +0530

    ex12 commit

---
+++ ex12.py
@@ -0,0 +1,6 @@
+# Promting People
+
+age = input("How old are you?\n")
+height = input("How tall are you? ")
+weight = input("What is your weight? ")
+print(f"My age is {age} and my height is {height} and I am {weight} heavy")
