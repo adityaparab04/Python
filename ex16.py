bea42961901196d9c0bd2a3b18bad421479537ac
adityaparab04 <adityaparab04@gmail.com>
2021-05-23 22:04:32 +0530

    ex16 final comit

--- ex16.py
+++ ex16.py
@@ -16,3 +16,21 @@ print("Truncating the file. Goodbye!")
 target.truncate()
 
 print("Now I'm going to ask you for 3 lines.")
+
+line1 = input("line 1: ")
+line2 = input("line 2: ")
+line3 = input("line 3: ")
+
+print("I am going to write these to the file.")
+
+target.write(line1)
+target.write("\n")
+
+target.write(line2)
+target.write("\n")
+
+target.write(line3)
+target.write("\n")
+
+print("And finally, we close it.")
+target.close()
