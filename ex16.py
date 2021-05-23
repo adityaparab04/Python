c917dd52168de84605efce55262804301a30e45a
adityaparab04 <adityaparab04@gmail.com>
2021-05-23 22:00:27 +0530

    ex16 initial commit

---
+++ ex16.py
@@ -0,0 +1,18 @@
+#Reading and writing Files
+from sys import argv
+
+script, filename = argv
+
+print("We're going to erase {filename}.")
+print("If you don't want that, hit CTRL-C (^C).")
+print("If you don't want that, hit RETURN.")
+
+input("?")
+
+print("Opening the file...")
+target = open(filename, 'w')
+
+print("Truncating the file. Goodbye!")
+target.truncate()
+
+print("Now I'm going to ask you for 3 lines.")
