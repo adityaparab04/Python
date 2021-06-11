795a657d8320019922f7d1c9a16b1c3d04d43e01
adityaparab04 <adityaparab04@gmail.com>
2021-06-11 23:32:56 +0530

    ex18 started

---
+++ ex18.py
@@ -0,0 +1,8 @@
+#names variables code fucntions
+
+#this one is like your scripts with argv
+def print_two(*args):
+    arg1, arg2 = args
+    print(f"arg1: {arg1}, arg2: {arg2}")
+
+#that *arg is actually pointless instead do this
