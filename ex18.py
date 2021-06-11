4d51b3c423c0ca3d604a869470adb4a5f0cdc45e
adityaparab04 <adityaparab04@gmail.com>
2021-06-11 23:38:50 +0530

    Ex18 completed

--- ex18.py
+++ ex18.py
@@ -6,3 +6,18 @@ def print_two(*args):
     print(f"arg1: {arg1}, arg2: {arg2}")
 
 #that *arg is actually pointless instead do this
+def print_two_again(arg1, arg2):
+    print(f"arg1: {arg1}, arg2: {arg2}")
+
+#this takes only one argument
+def print_one(arg1):
+    print(f"arg1: {arg1}")
+
+#this takes no arg
+def print_none():
+    print("I got none")
+
+print_two("adi", "ram")
+print_two_again("adi", "ram")
+print_one("Jack")
+print_none()
