9ff96badc3ce8e344238a6a3cd9f6b1092fce83a
adityaparab04 <adityaparab04@gmail.com>
2021-06-13 18:16:47 +0530

    ex21 done

---
+++ ex21.py
@@ -0,0 +1,26 @@
+def add(a, b):
+    print(f"Adding {a} + {b}")
+    return a + b
+
+def subtract(a, b):
+    print(f"Subtracting {a} - {b}")
+    return a - b
+
+def multiply(a, b):
+    print(f"Multiplying {a} * {b}")
+    return a * b
+
+def divide(a, b):
+    print(f"Dividing {a} / {b}")
+    return a / b
+
+print("Let's do some maths with just these funtions")
+
+what = add(subtract(10, 5), multiply(5, 4))
+print(f"this is {what}")
+
+
+
+
+
+
